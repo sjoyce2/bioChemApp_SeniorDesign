0152dd40e2db43fe506d8d636ed32cbd8f28f94a
Garett Palm <36228198+garettPalm@users.noreply.github.com>
2018-11-18 19:10:41 -0800

    Add files via upload

--- testApp/views.py
+++ testApp/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from testApp.models import User
+from django.contrib.auth import authenticate
 
 def modelChoice(request):
 	context = {}

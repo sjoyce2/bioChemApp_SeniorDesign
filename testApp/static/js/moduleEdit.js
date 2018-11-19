d002d3309cd974cb3c2b6ed6e4fea13f0647c311
kaylaLarson2018 <kayla.larson118@gmail.com>
2018-11-18 17:28:38 -0800

    added options to enter rxn weight and reversible rxns

--- testApp/static/js/moduleEdit.js
+++ testApp/static/js/moduleEdit.js
@@ -1,18 +1,37 @@
 "use strict";
 
-var checkedProdsEnzsSubs = [];
-var checkedProdsEnzsSubsNames = [];
-var canvas1;
-var substrates;
-var enzymes;
-var products;
-var ctx;
-var image;
-var createBtn;
-var objectWidth = 125;
-var objectHeight = 125;
-var objectBuffer = 10;
-//check which radio buttons are clicked
+var checkedProdsEnzsSubs = []; //array of types (i.e. enzyme, product or substrate)
+var checkedProdsEnzsSubsNames = []; //array of names of enzymes, products, substrates in rxn
+var canvas1; //variable for canvas element
+var substrates; //holds array of all substrate checkboxes
+var enzymes; //holds array of all enzyme radio buttons
+var products; //holds array of all product checkboxes
+var ctx; //context of the canvas element, is 2d
+var image; //image to be displayed corresponding to substrates and products chosen
+var createBtn; //button hit after module settings are set (displays rxn in canvas)
+var isReversible; //boolean that indicates if user wants rxn to be reversible or not
+var reversibleChoice; //variable to hold reversible radio buttons
+var weightSliderValue; //var corresponding to textbox for weight slider, displays weight value and allows user to set weight
+var submitWeightBtn; //var corresponding to button to enter weight set in text weightSliderValue textbox
+var weightSlider; //var corresponding to weight slider
+var objectWidth = 125; //width of boxes for substrates and products and arrows
+var objectHeight = 125; //height of boxes for substrates and products
+var objectBuffer = 10; //buffer size between each substrate, product and enzyme in canvas
+
+//function to change the text box to the value set by slider
+function updateTextInput(val) {
+  document.getElementById('weightSliderValue').value=val;
+}
+//function to change reversible boolean depending on which reversible radio btn is set
+function onRadioReverseChange(){
+  if(reversibleChoice[0].checked){
+    isReversible = true;
+  }else{
+    isReversible = false;
+  }
+  console.log(isReversible);
+}
+//check which radio buttons and checkboxes are clicked for products, enzymes and substrates
 function onRadioChange(){
   checkedProdsEnzsSubs = [];
   checkedProdsEnzsSubsNames = [];
@@ -35,7 +54,7 @@ function onRadioChange(){
     }
   }
 }
-
+//not used as of right now
 function getMousePosition(canvas1, event) {
     let border = canvas1.getBoundingClientRect();
     return {
@@ -43,13 +62,13 @@ function getMousePosition(canvas1, event) {
         y: event.clientY - border.top
     };
 }
-
+//display image corresponding to substrates and products chosen
 function displayImage(xcoor, ycoor, name){
   console.log(name);
   var img = document.getElementById(name);
   ctx.drawImage(img, xcoor, ycoor, objectWidth, objectHeight);
 }
-
+//draw boxes for substrates and products and set text for substrates, products and enzymes
 function drawObject(count, type, xcoor, ycoor, name){
   if(type === "substrate" || type === "product"){
     displayImage(xcoor, ycoor, name);
@@ -59,25 +78,40 @@ function drawObject(count, type, xcoor, ycoor, name){
     ctx.lineTo(xcoor, ycoor + objectHeight);
     ctx.lineTo(xcoor, ycoor);
     ctx.stroke();
+    ycoor = ycoor + objectHeight / 25; //add a small buffer so that text below image does not overlap with square
     ctx.font = "12px Arial";
     ctx.fillText(name, xcoor, ycoor + objectHeight + objectBuffer);
   }else if (type === "enzyme"){
     ctx.font = "12px Arial";
-    ctx.fillText(name, xcoor, ycoor + objectHeight / 2 + objectBuffer);
+    ycoor = ycoor + objectHeight / 25;
+    ctx.fillText(name, xcoor + (objectWidth / 4) + objectBuffer / 2, ycoor + objectHeight / 2 + objectBuffer);
   }else{
   }
 }
-
+//draw arrow for reaction, reversible or irreversible
 function drawArrow(xcoor, ycoor){
-  ycoor = ycoor + objectHeight / 2;
-  ctx.moveTo(xcoor, ycoor);
-  ctx.lineTo(xcoor + objectWidth, ycoor);
-  ctx.lineTo(xcoor + objectWidth - (objectWidth / 4), ycoor - (objectHeight / 4));
-  ctx.lineTo(xcoor + objectWidth - (objectWidth / 4), ycoor + (objectHeight / 4));
-  ctx.lineTo(xcoor + objectWidth, ycoor);
-  ctx.stroke();
+  if(isReversible){
+    ycoor = ycoor + objectHeight / 2;
+    ctx.moveTo(xcoor, ycoor);
+    ctx.lineTo(xcoor + (objectWidth / 4), ycoor - (objectHeight / 4));
+    ctx.lineTo(xcoor + (objectWidth / 4), ycoor + (objectHeight / 4));
+    ctx.lineTo(xcoor, ycoor);
+    ctx.lineTo(xcoor + objectWidth, ycoor);
+    ctx.lineTo(xcoor + objectWidth - (objectWidth / 4), ycoor - (objectHeight / 4));
+    ctx.lineTo(xcoor + objectWidth - (objectWidth / 4), ycoor + (objectHeight / 4));
+    ctx.lineTo(xcoor + objectWidth, ycoor);
+    ctx.stroke();
+  }else{
+    ycoor = ycoor + objectHeight / 2;
+    ctx.moveTo(xcoor, ycoor);
+    ctx.lineTo(xcoor + objectWidth, ycoor);
+    ctx.lineTo(xcoor + objectWidth - (objectWidth / 4), ycoor - (objectHeight / 4));
+    ctx.lineTo(xcoor + objectWidth - (objectWidth / 4), ycoor + (objectHeight / 4));
+    ctx.lineTo(xcoor + objectWidth, ycoor);
+    ctx.stroke();
+  }
 }
-
+//draw plus sign between each substrate and product
 function drawPlus(xcoor, ycoor){
   xcoor = xcoor + objectWidth + objectBuffer;
   ycoor = ycoor + objectHeight / 2;
@@ -87,26 +121,32 @@ function drawPlus(xcoor, ycoor){
   ctx.lineTo(xcoor + 5, ycoor - 5);
   ctx.stroke();
 }
-
+//called when create button is clicked
 function displayReaction(){
   var name;
-  console.log("DISPLAY REACTION CALLED");
-  console.log(checkedProdsEnzsSubs);
+  // console.log("DISPLAY REACTION CALLED");
+  // console.log(checkedProdsEnzsSubs);
   var currentX = objectBuffer;
   var currentY = objectBuffer;
+  //iterate through checkedProdsEnzsSubs array
   for(var a = 0; a < checkedProdsEnzsSubs.length; a++){
     name = checkedProdsEnzsSubsNames[a];
+    //reset currentX and currentY if over 4 products and substrates are being displayed
+    //so that everything is visible inside canvas border
     if(a > 0 && a % 5 === 0){
       currentX = objectBuffer;
       currentY = currentY + objectHeight + objectBuffer * 2;
     }
     var currentObject = checkedProdsEnzsSubs[a];
+    //check if currentObject is a Substrate
     if(currentObject === "Substrate"){
       drawObject(a, "substrate", currentX, currentY, name);
       if(checkedProdsEnzsSubs.length - 1 > a){
+        //if next object is an enzyme draw arrow
         if(checkedProdsEnzsSubs[a+1] === "Enzyme"){
           currentX = currentX + objectWidth + objectBuffer * 2;
           drawArrow(currentX, currentY);
+        //if next object is not an enzyme draw plus
         }else{
           drawPlus(currentX, currentY);
           currentX = currentX + objectWidth + objectBuffer * 2;
@@ -116,11 +156,13 @@ function displayReaction(){
       drawObject(a, "enzyme", currentX, currentY, name);
       console.log(currentObject);
       if(checkedProdsEnzsSubs.length - 1 > a){
+        //if next object is a product set currentX
         if(checkedProdsEnzsSubs[a+1] === "Product"){
           currentX = currentX + objectWidth + objectBuffer * 2;
         }
       }
     }else{
+      //draw all products
       drawObject(a, "product", currentX, currentY, name);
       if(checkedProdsEnzsSubs.length - 1 > a){
         drawPlus(currentX, currentY);
@@ -136,24 +178,45 @@ window.onload = function init(){
   substrates  = document.getElementsByName('Substrate');
   enzymes = document.getElementsByName('Enzyme');
   products = document.getElementsByName('Product');
+  reversibleChoice = document.getElementsByName('reversibleChoice');
   ctx  = canvas1.getContext("2d");
   image  = document.getElementById("step1");
   createBtn = document.getElementById("createReaction");
+  weightSliderValue = document.getElementById('weightSliderValue');
+  submitWeightBtn = document.getElementById('submitWeight');
+  weightSlider = document.getElementById('weightSlider');
 
   createBtn.addEventListener("click", function(event){
+    //clear everything out of canvas on button click so we can draw new rxn
     ctx.clearRect(0, 0, canvas1.width, canvas1.height);
+    //calling beginPath here preps canvas for drawing
     ctx.beginPath();
     console.log("button clicked");//show reaction
     console.log(checkedProdsEnzsSubs);
     displayReaction();
   });
 
-  canvas1.addEventListener("click", function (event) {
-      let mousePosition = getMousePosition(canvas1, event);
-      let rectNum = (Math.floor(mousePosition.x / 100) + Math.floor(mousePosition.y / objectWidth) * 8) - 15;
-      let rectClicked = document.getElementById("rectClicked");
-      console.log("CLICK");
-  });
+//not currently important
+  // canvas1.addEventListener("click", function (event) {
+  //     let mousePosition = getMousePosition(canvas1, event);
+  //     let rectNum = (Math.floor(mousePosition.x / 100) + Math.floor(mousePosition.y / objectWidth) * 8) - 15;
+  //     let rectClicked = document.getElementById("rectClicked");
+  //     console.log("CLICK");
+  // });
+
+//button to submit value entered in weight value text box
+//and set slider accordingly
+  submitWeightBtn.addEventListener("click", function(event) {
+    var val = weightSliderValue.value;
+    console.log(val);
+    //verify that value entered is valid and within range
+    if(val >= -1.0 && val <= 1.0){
+      weightSlider.value=val;
+    }else{
+      weightSliderValue.value=0.0;
+      weightSlider.value = 0.0;
+    }
+ });
 }
 function main () {
 }

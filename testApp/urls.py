3215b7f5d7591abf41cbf93996902197734ec951
Garett Palm <36228198+garettPalm@users.noreply.github.com>
2018-11-18 20:53:30 -0800

    Add files via upload

--- testApp/urls.py
+++ testApp/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from django.cong.urls import url
 from testApp import views
 from django.contrib.auth.views import login
 
 urlpatterns = [
-	path('', views.display, name= 'display'),
+	path('', views.display, name= 'index'),
 	path('', views.login, name = 'login'),
 	path('', views.signup, name = 'signup'),
 	path('moduleEdit', views.moduleEdit, name='moduleEdit'),

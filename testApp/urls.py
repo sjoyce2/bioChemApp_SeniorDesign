6bc28ae2e0d80bcf7681afa3cec72479be266827
Garett Palm <36228198+garettPalm@users.noreply.github.com>
2018-11-18 20:50:35 -0800

    Update urls.py

--- testApp/urls.py
+++ testApp/urls.py
@@ -4,7 +4,7 @@ from testApp import views
 from django.contrib.auth.views import login
 
 urlpatterns = [
-	path('', views.display, name= 'index'),
+	path('', views.display, name= 'display'),
 	path('', views.login, name = 'login'),
 	path('', views.signup, name = 'signup'),
 	path('moduleEdit', views.moduleEdit, name='moduleEdit'),

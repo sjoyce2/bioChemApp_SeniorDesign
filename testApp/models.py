b6d94f2b9d701f20d21ca9b3c97d2a0f8955daab
Garett Palm <36228198+garettPalm@users.noreply.github.com>
2019-01-28 16:09:18 -0800

    Update models.py

--- testApp/models.py
+++ testApp/models.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.contrib.auth.models import User
 from django.db import models
 
-class User(models.Model):
-	username = models.CharField(max_length=20)
-	password = models.CharField(max_length=20)
-
-	def __str__(self):
-		return self.username
+class Module(models.Model):
+	userID = models.ForeignKey(User, on_delete=models.CASCADE)
+	modelID = models.IntegerField()
+	moduleID = models.IntegerField()
+	substraight = models.CharField(max_length = 200 )
+	product = models.CharField(max_length = 200 )
+	enzyme = models.CharField(max_length = 200 )
+	reversible = models.CharField(max_length = 200 )
+	modelName = models.CharField(max_length = 200 )

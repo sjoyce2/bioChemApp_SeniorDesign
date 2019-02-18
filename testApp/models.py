da54aa51f84f87d5b61c0032732709a794df8a42
sjoyce2 <sjoyce2@zagmail.gonzaga.edu>
2019-02-18 22:36:40 +0000

    renaming to moduleID attribute in products and substrates

--- testApp/models.py
+++ testApp/models.py
@@ -14,22 +14,27 @@ class Model(models.Model):
 class Module(models.Model):
 	modelID = models.ForeignKey(Model, on_delete=models.CASCADE)
 	enzyme = models.CharField(max_length = 200 )
+	enzymeAbbr = models.CharField(max_length = 8, default="ENZYME")
 	reversible = models.CharField(max_length = 200 )
+	xCoor = models.IntegerField(default=0)
+	yCoor = models.IntegerField(default=0)
 
 	def __str__(self):
 		return_val = str(id) + str(self.modelID) + str(self.enzyme) + str(self.reversible)
 		return return_val
 
 class Products(models.Model):
-	module = models.ForeignKey(Module, on_delete=models.CASCADE)
+	moduleID = models.ForeignKey(Module, on_delete=models.CASCADE)
 	product = models.CharField(max_length = 200)
+	abbr = models.CharField(max_length = 8, default="PROD")
 
 	def __str__(self):
-		return str(id) + str(self.module) + str(self.product)
+		return str(id) + str(self.moduleID) + str(self.product)
 
 class Substrates(models.Model):
-	module = models.ForeignKey(Module, on_delete=models.CASCADE)
+	moduleID = models.ForeignKey(Module, on_delete=models.CASCADE)
 	substrate = models.CharField(max_length = 200)
+	abbr = models.CharField(max_length = 8, default="SUB")
 	
 	def __str__(self):
-		return str(id) + str(self.module) + str(self.substrate)
+		return str(id) + str(self.moduleID) + str(self.substrate)

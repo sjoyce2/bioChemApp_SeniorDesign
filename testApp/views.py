dcf094cf5e536e9953bdd82aef1322ccf0c99452
kaylaLarson2018 <kayla.larson118@gmail.com>
2019-02-18 15:39:44 -0800

    update

--- testApp/views.py
+++ testApp/views.py
@@ -3,18 +3,22 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from .models import Module
+from django.contrib.auth.models import User
 
 # Create your views here.
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 #from testApp.models import User
 from django.contrib.auth import authenticate
 from django.contrib.auth import login as auth_login
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from testApp.forms import SignUpForm
+from testApp.forms import SaveModuleForm
 from testApp.models import Module
-#from testApp.models import Products
-#from testApp.models import Substrates
+from testApp.models import Model
+from testApp.models import Products
+from testApp.models import Substrates
 
 def modelChoice(request):
 	# need to add a pop-up/redirect to allow the user to create a new model
@@ -73,30 +77,29 @@ def register(request):
 
 def login(request):
 	context = {
-	}
-	return render(request, 'registration/login.html', context=context)
 
-def load(request):
-	context = {
 	}
 	return render(request, 'load.html', context=context)
 
-def createModule(new_userId, new_modelId, new_moduleId, substrate_list, product_list, new_enzyme, new_reversible, new_modelName):
+def signup(request):
+	context = {
+	}
+	return render(request, 'accounts/signup.html', context=context)
 
+'''def createModule(new_userId, new_modelId, new_moduleId, substrate_list, \
+	product_list, new_enzyme, new_reversible, new_modelName):
 	new_module = Module(userID = new_userId, modelID = new_modelId, \
 		moduleID = new_moduleId, enzyme = new_enzyme, reversible = \
 		new_reversible, modelName = new_modelName)
 	new_module.save()
-
 	for i in range(len(substrate_list)):
 		new_substrate = Substrates(module = new_moduleId, substrate = \
 			substrate_list[i])
 		new_substrate.save()
-
 	for i in range(len(product_list)):
 		new_product = Products(module = new_moduleId, product = \
 			product_list[i])
-		new_product.save()
+		new_product.save() '''
 
 
-	return render(request, 'accounts/display.html', context=context)
+#def readModule(id):

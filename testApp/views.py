61211a77145959fbd5243d081bdb9a32dffe643f
Garett Palm <36228198+garettPalm@users.noreply.github.com>
2019-02-18 15:27:40 -0800

    Add files via upload

--- testApp/views.py
+++ testApp/views.py
@@ -2,12 +2,19 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from .models import Module
 
 # Create your views here.
 from django.http import HttpResponse
-from testApp.models import User
+#from testApp.models import User
 from django.contrib.auth import authenticate
-
+from django.contrib.auth import login as auth_login
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from testApp.forms import SignUpForm
+from testApp.models import Module
+#from testApp.models import Products
+#from testApp.models import Substrates
 
 def modelChoice(request):
 	context = {}
@@ -23,36 +30,47 @@ def modelEdit(request):
 	}
 	return render(request, 'modelEdit.html', context=context)
 
-def register(request):
-	context = {
-	}
-	return render(request, 'registration/register.html', context=context)
+def signup(request):
+	if request.method == 'POST':
+		form = SignUpForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username = form.cleaned_data.get('username')
+			raw_password = form.cleaned_data.get('password1')
+			user = authenticate(username=username, password=raw_password)
+			auth_login(request, user)
+			return redirect('modelChoice')
+	else:
+		form = SignUpForm()
+	return render(request, 'registration/signup.html', {'form': form})
 
 def login(request):
 	context = {
+	}
+	return render(request, 'registration/login.html', context=context)
 
+def load(request):
+	context = {
 	}
 	return render(request, 'load.html', context=context)
 
-def signup(request):
-	context = {
+def createModule(new_userId, new_modelId, new_moduleId, substrate_list, product_list, new_enzyme, new_reversible, new_modelName):
+	
+	new_module = Module(userID = new_userId, modelID = new_modelId, \
+		moduleID = new_moduleId, enzyme = new_enzyme, reversible = \
+		new_reversible, modelName = new_modelName)
+	new_module.save()
 
-	}
-	return render(request, 'accounts/signup.html', context=context)
+	for i in range(len(substrate_list)):
+		new_substrate = Substrates(module = new_moduleId, substrate = \
+			substrate_list[i])
+		new_substrate.save()
 
-def display(request):
-	objects = User.objects.get(username='user1')
-	exists = False
+	for i in range(len(product_list)):
+		new_product = Products(module = new_moduleId, product = \
+			product_list[i])
+		new_product.save()
 
-	#if User.objects.filter(username=user1).exists():
-	#	exists = True
 
-	context = {
-		'user_name': objects.username,
-		'pass_word': objects.password,
-		'userExists': exists,
-	}
-
-	return render(request, 'accounts/display.html', context=context)
 
 	

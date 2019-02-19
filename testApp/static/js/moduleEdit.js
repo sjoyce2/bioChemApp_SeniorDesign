71a68a606b8455e3d1c57497cddb695257172680
kaylaLarson2018 <kayla.larson118@gmail.com>
2019-02-18 16:07:35 -0800

    check local storage

--- testApp/static/js/moduleEdit.js
+++ testApp/static/js/moduleEdit.js
@@ -571,6 +571,7 @@ window.onload = function init(){
   var saveBtnClicked = false;
   //SET CURRENT REACTION TO STRING FROM MODEL EDIT SCREEN HERE
   var fromModel = localStorage.getItem("reactionClicked");
+  console.log("LOCAL STORAGE FROM MODEL EDIT ");
   console.log(fromModel);
   currentRxn = step3;//set currentRxn to dummy value
   setArraysRepresentingReaction();//function to set the arrays to currentRxn and call values to display/validate

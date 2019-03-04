55ebc9f48d384cfa00a18990d757d8f4353f6042
kaylaLarson2018 <kayla.larson118@gmail.com>
2019-03-03 18:34:43 -0800

    parameters in url

--- testApp/views.py
+++ testApp/views.py
@@ -37,8 +37,8 @@ def modelChoice(request):
 
 def moduleEdit(request, module):
 	print ("HI");
-	mod = Module.objects.all().filter(pk = module).values('modelID_id')
-	mod = Module.objects.all()
+	currentMod = Module.objects.all().filter(pk = module).values('modelID_id')
+	mod = Module.objects.all().filter(modelID_id = currentMod)
 	subs = Substrates.objects.all().filter(moduleID_id__exact = module)
 	prods = Products.objects.all().filter(moduleID_id__exact = module)
 	# mod = Module.objects.all()

0edc4b4678e4d9bf98f6217ce546a4db1fba8cb3
kaylaLarson2018 <kayla.larson118@gmail.com>
2019-04-08 17:19:22 -0700

    url patterns for password reset

--- testApp/urls.py
+++ testApp/urls.py
@@ -8,6 +8,12 @@ from django.contrib.auth.views import LoginView
 
 
 urlpatterns = [
+	# url('^', include('django.contrib.auth.urls')),
+	url(r'^password_reset/$', auth_views.PasswordResetView, name='password_reset'),
+    url(r'^password_reset/done/$', auth_views.PasswordResetDoneView, name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        auth_views.PasswordResetConfirmView, name='password_reset_confirm'),
+    url(r'^reset/done/$', auth_views.PasswordResetCompleteView, name='password_reset_complete'),
 	path('', views.login, name= 'login'),
 	path('accounts/', include('django.contrib.auth.urls')),
 	path('moduleEdit/<int:model>/<int:module>/<int:xCoor>/<int:yCoor>/<int:isPositive>', views.moduleEdit, name='moduleEdit'),
@@ -16,5 +22,5 @@ urlpatterns = [
 	path('register', views.register, name='register'),
 	path('home', views.home, name='home'),
 	path('indexLogged', views.indexLogged, name='indexLogged'),
-	path('logout_view', views.logout_view, name='logout_view'),
+	path('logout_view', views.logout_view, name='logout_view')
 ]
